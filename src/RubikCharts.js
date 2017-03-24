3bf1e80f5aea35ba32868b5f5a22497dc903bb37
rinti <hello@rinti.se>
2017-03-24 23:41:18 +0100

    fix the goddamn charts!

--- src/RubikCharts.js
+++ src/RubikCharts.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import {ResponsiveContainer, BarChart, Bar} from 'recharts'
+import {ResponsiveContainer, BarChart, Bar, Cell} from 'recharts'
 
 class RubikCharts extends Component {
   render() {
     return (
       <ResponsiveContainer>
-        <BarChart height={100} data={this.props.data}>
-          <Bar dataKey="passedTime" fill="#ad3957" />
+        <BarChart stackOffset='wiggle' height={100} data={this.props.data}>
+          <Bar dataKey="passedTime" fill="#ad3957" isAnimationActive={false}>
+              {
+                this.props.data.map((item, i) => {
+                  return (<Cell key={`test{i}`} fill="#ad3957" />)
+                })
+              }
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     )
